88ee369da091d7cfb205f589a4200c224c493081
prodigy803 <pushkaraj803@gmail.com>
2018-11-22 00:44:50 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,5 +1,7 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
@@ -8,4 +10,23 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(df=data):
+    rfe = RandomForestClassifier()
+    X,y = data.iloc[:,:-1],data.iloc[:,-1]
+
+    selector = RFE(rfe, step=1)
+    selector = selector.fit(X, y)
+
+    arr_columns = np.array(data.columns)
+    arr = np.select([selector.get_support()],[arr_columns[:-1]] )
+    return [arr[arr!=0]]
+
+
+
+
+
+
+
+
+
 
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -8,3 +9,17 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(df):
+    
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+    rf = RandomForestClassifier()
+    slm = SelectFromModel(rf)
+    slm.fit(X,y)
+    slm.get_support()
+    columns = np.array(df.columns[:-1])
+    return columns[slm.get_support()[:]].tolist()
+
+
+
+
